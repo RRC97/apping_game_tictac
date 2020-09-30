cc169b85bbfb91da4905af26192ae57bb708a5e1
RRC97 <matheuspereira.nave@gmail.com>
2020-09-29 22:32:09 -0300

    Partidas com mais de uma rodada e vencedor por mais rodadas ganhas

--- src/migrations/20200923194238_create_table_matches.js
+++ src/migrations/20200923194238_create_table_matches.js
@@ -6,10 +6,14 @@ exports.up = function(knex) {
         table.integer('request_id').notNullable();
         table.foreign('request_id').references('Match_requests:id');
         
-        table.integer('winner').defaultTo(0);
+        table.string('winner').defaultTo('[]');
         table.integer('turn').defaultTo(0);
-        table.string('data').defaultTo('[[0,0,0],[0,0,0],[0,0,0]]');
+        table.text('data').defaultTo('[[[0,0,0],[0,0,0],[0,0,0]]]');
+        table.integer('index').defaultTo(0);
+        table.integer('length').defaultTo(1);
         table.text('chat').defaultTo('[]');
+        table.text('moves').defaultTo('[]');
+        table.integer('result').defaultTo(0);
 
         table.integer('status').defaultTo(0);
         table.integer('type').defaultTo(0);
--- src/server.js
+++ src/server.js
@@ -12,14 +12,25 @@ const onlinePlayers = [
 
 ];
 
-function isValidMove(data, x, y) {
-    const board = JSON.parse(data);
+function createBoard() {
+    const board = [];
+
+    for(var i = 0; i < 3; i++) {
+        var line = []
+        for(var j = 0; j < 3; j++) {
+            line.push(0);
+        }
+        board.push(line);
+    }
+    return board;
+}
+
+function isValidMove(board, x, y) {
     return (board[x][y] == 0);
 }
 function doMove(data, x, y, player) {
-    const board = JSON.parse(data);
-    board[x][y] = player;
-    return board;
+    data[x][y] = player;
+    return data;
 }
 function isWinner(board) {
     var valueDiag1 = 0;
@@ -77,9 +88,30 @@ io.on('connection', socket => {
                         socket.join(match.id);
                     }
                 } else {
+                    var length = 1;
+
+                    switch(match_request.type) {
+                        case 1:
+                            length = 3;
+                            break;
+                        case 2:
+                            length = 5;
+                            break;
+                        case 3:
+                            length = 10;
+                            break;
+                        case 4:
+                            length = 1;
+                            break;
+                        case 5:
+                            length = 1;
+                            break;
+                    }
                     const match = await database('matches').returning('id').insert({
                         request_id: match_request.id,
-                        type: match_request.type
+                        id: match_request.id,
+                        type: match_request.type,
+                        length: length
                     });
                     if(match.length > 0) {
                         socket.emit('token:success', onlinePlayers[socket.id]);
@@ -92,19 +124,47 @@ io.on('connection', socket => {
             socket.emit('token:failed', 'Token inexistente');
         }
     });
-    socket.on('chat-message', message => {
+    socket.on('chat-message', async message => {
         const player = onlinePlayers[socket.id];
-        io.sockets.in(player.room).emit('chat-message', {from: player, message: message});
+        const match = await database('matches')
+            .where('id', player.room).returning(['chat']);
+        if(match.length > 0) {
+            const chat = JSON.parse(match[0].chat);
+            chat.push({from: player.id, message: message});
+            await database('matches')
+                .where('id', player.room)
+                .update({chat: JSON.stringify(chat)});
+
+            io.sockets.in(player.room).emit('chat-message', {from: player, message: message});
+        }
     });
     socket.on('game-update', async () => {
         const player = onlinePlayers[socket.id];
-        const match = await database('matches')
+        const response = await database('matches')
             .where('id', player.room);
 
-        if(match.length > 0) {
-            //console.log(match[0].data);
-            match[0].creator = player.creator;
-            socket.emit('game-update', match[0]);
+        if(response.length > 0) {
+            var match = response[0];
+            players = io.sockets.in(player.room).sockets;
+            for(var socketPlayer in players) {
+                if(onlinePlayers[socketPlayer] == player) {
+                    if(player.creator) {
+                        match.creator = player;
+                    } else {
+                        match.oponent = player;
+                    }
+                } else {
+                    if(!player.creator) {
+                        match.creator = onlinePlayers[socketPlayer];
+                    } else {
+
+                        match.oponent = onlinePlayers[socketPlayer];
+                    }
+                }
+            };
+            match.current = JSON.parse(match.data)[match.index];
+            match.isCreator = player.creator;
+            socket.emit('game-update', match);
         }
     });
     socket.on('game-move', async move => {
@@ -112,20 +172,83 @@ io.on('connection', socket => {
         const match = await database('matches')
             .where('id', player.room);
         if(match.length > 0) {
-            if(match[0].winner == 0) {
+            if(match[0].status < 2) {
                 if((player.creator && match[0].turn == 0)
                 || (!player.creator && match[0].turn == 1)) {
-                    if(isValidMove(match[0].data, move.x, move.y)) {
-                        const board = doMove(match[0].data, move.x, move.y, player.creator ? 1 : 2);
+                    const rounds = JSON.parse(match[0].data);
+                    if(isValidMove(rounds[match[0].index], move.x, move.y)) {
+                        const board = doMove(rounds[match[0].index], move.x, move.y, player.creator ? 1 : 2);
+                        
                         const winnerResult = isWinner(board);
-                        const winner = winnerResult > 0 ? winnerResult : 0;
-                        await database('matches')
+                        const roundWinner = winnerResult > 0 ? winnerResult : 0;
+
+                        var winner = JSON.parse(match[0].winner);
+                        var status = match[0].status;
+                        var index = match[0].index;
+                        var matchResult = 0;
+                        if(roundWinner) {
+                            winner[match[0].index] = roundWinner;
+
+                            var winnerCreator = 0;
+                            var winnerOponent = 0;
+
+                            for(var i = 0; i < winner.length; i++) {
+                                console.log(winner[i]);
+                                if(winner[i] == 1) {
+                                    winnerCreator++;
+                                } else if(winner[i] == 2) {
+                                    winnerOponent++;
+                                }
+                            }
+                            if(winnerCreator > match[0].length / 2) {
+                                status++;
+                                matchResult = 1;
+                            } else if(winnerOponent > match[0].length / 2) {
+                                status++;
+                                matchResult = 2;
+                            } else {
+                                index++;
+                                rounds.push(createBoard());
+                            }
+                        }
+
+                        const moves = JSON.parse(match[0].moves);
+                        moves.push({index: match[0].index, player: player.id, move: {x: move.x, y: move.y}});
+
+                        const response = await database('matches')
                             .where('id', match[0].id)
                             .update({
-                                data: JSON.stringify(board),
-                                winner: winner,
-                                turn: player.creator ? 1 : 0
-                            });
+                                winner: JSON.stringify(winner),
+                                moves: JSON.stringify(moves),
+                                data: JSON.stringify(rounds),
+                                turn: player.creator ? 1 : 0,
+                                status: status,
+                                index: index,
+                                result: matchResult
+                            }).returning('*');
+
+                        if(response.length > 0) {
+                            var matchUpdate = response[0];
+                            players = io.sockets.in(player.room).sockets;
+                            for(var socketPlayer in players) {
+                                if(onlinePlayers[socketPlayer] == player) {
+                                    if(player.creator) {
+                                        matchUpdate.creator = player;
+                                    } else {
+                                        matchUpdate.oponent = player;
+                                    }
+                                } else {
+                                    if(!player.creator) {
+                                        matchUpdate.creator = onlinePlayers[socketPlayer];
+                                    } else {
+                                        matchUpdate.oponent = onlinePlayers[socketPlayer];
+                                    }
+                                }
+                            }
+                            matchUpdate.current = JSON.parse(matchUpdate.data)[matchUpdate.index];
+                            matchUpdate.isCreator = player.creator;
+                            io.sockets.in(player.room).emit('game-update', matchUpdate);
+                        }
                     } else {
                         socket.emit('game-move:failed', {error: 0});
                     }
@@ -135,7 +258,6 @@ io.on('connection', socket => {
             } else {
                 socket.emit('game-move:failed', {error: 2, match: match[0]});
             }
-            //await database('matches').where('id', match[0].id).update({data: '[[0,0,0],[0,0,0],[0,0,0]]', winner: 0}); 
         }
     });
 });
